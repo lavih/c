ed00cbe452f9250b15544562838f4ba4a638c3a3
lavih <lavih44@walla.co.il>
2022-08-07 17:58:18 -0400

    Chore: update

---
+++ docs/week2-language-fundamentals/targil_mesakem_question_4.c
@@ -0,0 +1,13 @@
+/* Week 2 ex 4 */
+
+#include <stdio.h>
+
+int main()
+{
+	int num;
+
+	printf("Please enter a number between 0-1,000,000:\n");
+	scanf("%d",&num);
+    printf("your number raised to the second power is: %d\n",num*num);
+    return 0;
+}
---
+++ docs/week2-language-fundamentals/targil_mesakem_question_5.c
@@ -0,0 +1,14 @@
+/* Week 2 ex 5 */
+
+#include <stdio.h>
+
+int main()
+{
+    float num;
+    printf("Please enter a number:\n");
+    scanf("%f",&num);
+    printf("%f\n",&num);
+    printf("%.3f\n",&num);
+    return 0;
+}
+
