34335a0ba8fc2ad327c716c6203cb0d207386317
root <root@LAPTOP-VMTBE6AI.localdomain>
2022-09-19 09:40:52 +0300

    Chore: update

--- docs/week2-language-fundamentals/targil_mesakem_question_6.c
+++ docs/week2-language-fundamentals/targil_mesakem_question_6.c
@@ -8,8 +8,23 @@ int main()
     int i = 2;
     double d;
     // Place your code here:
+    // 1
     i = (i+6)/7;
-    printf("The number is: %i\n",i);
+    printf("The 1 number is: %i\n",i);
+    // 2
+    i = i - 7;
+    printf("The 2 number is: %i\n",i);
+    // 3
+    // 4
+    // 5
+    // 6
+    // 7
+    // 8
+    // 9
+    // 10
+    // 11
+    // 12
+    // 13
 
 
     return 0;
